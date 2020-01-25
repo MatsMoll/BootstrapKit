2a9d21c583deb3a01317eddab0cc83a2973bed27
Mats Eikeland Mollestad <mem@mollestad.no>
2020-01-25 15:15:19 +0100

    [Package] changed HTMLKit version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor-community/HTMLKit", from: "2.0.0-beta.3")
+        .package(url: "https://github.com/vapor-community/HTMLKit", from: "2.0.0-beta.3.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
